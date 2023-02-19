b3833cc8aff31baaa4f7f95cce587d36937cff90
Maksim Buryshynets <arhor18@gmail.com>
2023-02-19 11:03:52 +0300

    utils package structure refactored

---
+++ src/utils/createLazy.js
@@ -0,0 +1,17 @@
+/**
+ * Returns an object holding property 'value' that will be lazily evaluated using provided factory function.
+ * 
+ * @template T
+ * @param {() => T} factory 
+ * @returns {{ value: T }}
+ */
+export default function createLazy(factory) {
+    return {
+        get value() {
+            Object.defineProperty(this, 'value', {
+                value: factory(),
+            });
+            return this.value;
+        },
+    };
+}
